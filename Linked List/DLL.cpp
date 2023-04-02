ef28951de5087361eae3e688d0f7cdf94ec6d03c
karan123Sharma <karanjitendrasharma302@gmail.com>
2023-04-02 19:20:24 +0530

    main

--- Linked List/DLL.cpp
+++ Linked List/DLL.cpp
@@ -17,6 +17,22 @@ void Insertatfirst(Node *&head,int d){
     temp->next = head;
     head = temp; 
 }
+void InsertAtMiddle(Node *&head,int d,int pos){
+    Node * temp = head;
+    int count = 1;
+    while(count < pos-1){
+        temp = temp->next;
+        count++;
+    }
+    Node * newnode = new Node(d);
+    newnode->prev= temp;
+    newnode->next = temp->next;
+    temp->next = newnode;
+    temp = temp->next;
+    temp->prev = newnode;
+
+
+}
 void InsertAtLast(Node *&tail,int d){
     Node *temp = new Node(d);
     temp->prev = tail;
@@ -36,9 +52,13 @@ int main(){
     Node *tail = newnode;
     Insertatfirst(head,67);
     Insertatfirst(head,17);
+    Insertatfirst(head,70);
     InsertAtLast(tail,89);
     InsertAtLast(tail,80);
     InsertAtLast(tail,10);
+    InsertAtMiddle(head,56,3);
+    InsertAtMiddle(head,59,3);
+    InsertAtLast(tail,90);
     printlist(head);
 
 }
