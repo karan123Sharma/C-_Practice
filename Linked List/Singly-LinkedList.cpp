5aa58822e363c287379e3a54b08602818fa6d81b
karan123Sharma <karanjitendrasharma302@gmail.com>
2023-04-02 19:06:34 +0530

    main

--- Linked List/Singly-LinkedList.cpp
+++ Linked List/Singly-LinkedList.cpp
@@ -1,32 +1,66 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int data;
     Node *next;
-    Node(int data){
-        this->data= data;
+
+    Node(int data)
+    {
+        this->data = data;
         this->next = NULL;
     }
 };
-void InsertAtHead(Node * &head,int d){
+void InsertAtHead(Node *&head, int d)
+{
     Node *temp = new Node(d);
     temp->next = head;
     head = temp;
-    }
-void PrintList(Node * &head){
+}
+void InsertAtEnd(Node * &tail,int d){
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+void InsertatMiddle(Node *&head,Node *&tail,int d, int pos){
     Node *temp = head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
+    // if(pos==1){
+    //     InsertAtHead(head,d);
+    //     return;
+    // }
+    int count = 1;
+    while(count <  pos-1){
+        temp = temp->next;
+        count++;
+    }
+    if(temp->next == NULL){
+        InsertAtEnd(tail,d);
+        return;
+    }
+    Node * newnode = new Node(d);
+    newnode->next = temp->next;
+    temp->next = newnode;
+}
+void printlist(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
         temp = temp->next;
     }
-}    
-int main(){
-    Node *newnode=new Node(10);
-    Node *head = newnode;
-    InsertAtHead(head,34);
-    InsertAtHead(head,3);
-    InsertAtHead(head,40);
-    PrintList(head);
+}
+int main()
+{
+    Node *newNode = new Node(10);
+    Node *head = newNode;
+    Node *tail = newNode;
+    InsertAtHead(head,89);
+    InsertAtEnd(tail,90);
+    InsertAtEnd(tail,10);
+    InsertatMiddle(head,tail,12,1);
+     
+    printlist(head);
 
-;}
+}
